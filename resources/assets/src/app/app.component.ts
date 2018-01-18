3180f82587812b6b1532fa29e1f773b8c0aecb70
thangtt <ngo.trung.thang@framgia.com>
2018-01-18 17:18:35 +0700

    init

--- resources/assets/src/app/app.component.ts
+++ resources/assets/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
     <nav>
       <a routerLink="/login" routerLinkActive="active">Login</a>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/reset" routerLinkActive="active">reset</a>
     </nav>
     <router-outlet></router-outlet>
   `,
